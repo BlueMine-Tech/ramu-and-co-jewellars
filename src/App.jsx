fa84e7adf5200ca7d9edea722f8dacd7ff3244bb
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2025-12-03 11:25:40 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
-import Homepage from "./pages/homepage";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
 import Footer from "./components/Footer";
 
 function App() {
